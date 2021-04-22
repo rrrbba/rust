33f051575895268373197a26ab8bf8d4861ff4ec
Rayven <rayven.burns@gmail.com>
2021-04-21 22:22:37 -0400

    added comment

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,14 @@
 use std::io; //standard library
-
+use rand::Rng;
 
 
 fn main() {
 
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number); //this line only for testing
 
     println!("Please input your guess:");
 
