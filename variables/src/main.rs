f58761317426191b1b0245d931b1c991d3888dd3
Rayven <rayven.burns@gmail.com>
2021-04-27 20:53:32 -0400

    added function definition example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -11,5 +11,23 @@ fn main() {
     println!("The value of y is: {}", y);
     let six_four = tup.1; //accessing an element in a tuple directly
     println!("The value of the second element is: {}", six_four);
+
+    //Arrays
+    //name: [type; number of elements] = [array elements]
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+    //initializing an array that contain the same value for each element -> [initial value; number of elements] 
+    let c = [3; 5]; // [3, 3, 3, 3, 3]
+    //Accessing elements in an array
+    let c_second = c[1];
+    println!("The value of the second element in array c is: {}", c_second);
+
+    //Functions
+    println!("Hello");
+    another_function();
+}
+
+//Could've defined this function before the main function as well, rust doesn't care where
+fn another_function() {
+    println!("Another function!");
 }
 
