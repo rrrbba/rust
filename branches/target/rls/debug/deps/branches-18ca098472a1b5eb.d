5a046d709433ef527179e41f5f2eeb5b97d023a8
Rayven <rayven.burns@gmail.com>
2021-05-02 19:21:55 -0400

    added branches

---
+++ branches/target/rls/debug/deps/branches-18ca098472a1b5eb.d
@@ -0,0 +1,5 @@
+c:\Users\Rayven Wang\Documents\GitHub\rust\branches\target\rls\debug\deps\branches-18ca098472a1b5eb.rmeta: src\main.rs
+
+c:\Users\Rayven Wang\Documents\GitHub\rust\branches\target\rls\debug\deps\branches-18ca098472a1b5eb.d: src\main.rs
+
+src\main.rs:
---
+++ branches/target/rls/debug/deps/branches-65050303e7decc66.d
@@ -0,0 +1,5 @@
+c:\Users\Rayven Wang\Documents\GitHub\rust\branches\target\rls\debug\deps\branches-65050303e7decc66.rmeta: src\main.rs
+
+c:\Users\Rayven Wang\Documents\GitHub\rust\branches\target\rls\debug\deps\branches-65050303e7decc66.d: src\main.rs
+
+src\main.rs:
