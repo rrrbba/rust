558309d73a24845cdb86b1671da40d57c79afba5
Rayven <rayven.burns@gmail.com>
2021-05-05 21:45:23 -0400

    Added struct config and updated main and parse_config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,22 +9,27 @@ fn main() {
     // println!("{:?}", args); 
 
     //Passing the vector to parse_config
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     //Takes the filename, opens the file and returns a Result<String> of the file's contents
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-//Extracted functionality for parsing arguments (holds the logic for determining which argument goes in which variable)
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1]; //program name
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+//Extracted functionality for parsing arguments (holds the logic for determining which argument goes in which variable)
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1].clone(); //program name
+    let filename = &args[2].clone();
+
+    Config { query, filename }
 }
